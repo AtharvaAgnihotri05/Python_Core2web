5122cfa6d08cdc86b6bc239690d895e0b7de00ce
AtharvaAgnihotri05 <agnihotriatharva38@gmail.com>
2025-10-31 12:38:04 +0530

    Logical Ops

---
+++ Python/ClassCode/Operators/Program4.py
@@ -0,0 +1,14 @@
+# logical Operators
+
+x = 10
+print(x)
+y = ++x   # this operator will not work in python it consider it as "y + +x" and post increment is not supported in python
+
+y = 15
+z = 30
+
+print(x < y and y < z)   # and Operator
+print(x < y or y < z)    # or Operator
+print(not(x < y))        # not Operator
+
+
