e825fee2780789f592e6a5b2fb2e3bb35b665af7
AtharvaAgnihotri05 <agnihotriatharva38@gmail.com>
2025-11-04 19:31:19 +0530

    Operators

---
+++ Python/ClassCode/Operators/Program5.py
@@ -0,0 +1,17 @@
+# identity operators
+
+x = 300
+y = 300
+
+print(id(x))
+print(id(y))
+
+print(x == y)
+print(id(x) == id(y))
+
+str1 = "Atharva"
+str2 = "Atharva"
+
+print(id(str1))
+print(id(str2))
+print(str1 == str2)
